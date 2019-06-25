6dacc52cff95f1dac7ae4abc9881cf81527bc98b
Jesse Hao <jeasygates@hotmail.com>
2019-06-25 18:22:36 +0800

    init commit

---
+++ CI-TEST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CI-TEST
+//
+//  Created by Jesse Hao on 2019/6/25.
+//  Copyright © 2019 snoware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
